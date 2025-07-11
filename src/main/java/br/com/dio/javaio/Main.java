0a005baa0a3050836321ae60eb80af331721086d
DIEGO ALMEIDA <almeidadieo488@gmail.com>
2025-07-11 10:42:02 -0300

    Trabalhando com NIO , replace, remove , findBy, List

--- src/main/java/br/com/dio/javaio/Main.java
+++ src/main/java/br/com/dio/javaio/Main.java
@@ -7,14 +7,28 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         FilePersistence persitence = new NIOFilePersistence("user.csv");
-        System.out.println(persitence.write("bianca;bianca@bia.ocm;22/09/1997;"));
+        System.out.println(persitence.write("Bianca;bianca@bia.ocm;22/09/1997;"));
         System.out.println("===============");
-        System.out.println(persitence.write("bernardo;bernado@bernardo.com;28/11/1999;"));
+        System.out.println(persitence.write("Bernardo;bernado@bernardo.com;28/11/1999;"));
         System.out.println("===============");
         System.out.println(persitence.write("Ricaro;ricardo@ricardo.com;12/01/2000;"));
         System.out.println("===============");
         System.out.println(persitence.findAll());
+        System.out.println("================");
+        System.out.println(persitence.remove(";ricardo@"));
         System.out.println("===============");
+        System.out.println(persitence.remove(";joana@"));
+        System.out.println("===============");
+        System.out.println(persitence.findBy(";bia@"));
+        System.out.println("===============");
+        System.out.println(persitence.findBy(";ricardo@"));
+        System.out.println("===============");
+        System.out.println(persitence.replace("Bernardo","Diego;diego@diego.com;04/041993"));
+        System.out.println("===============");
+        System.out.println(persitence.replace(";joaquim" , "Diego;diego@diego.com;04/07/1993"));
+        System.out.println("================");
+
+
 //         FilePersistence persistence = new IOFilePersistence("user.csv");
 //        System.out.println("===============");
 //        System.out.println(persistence.write("Diego; diego@Diego.com ;04/07/1999 ;"));
--- src/main/java/br/com/dio/javaio/NIOFilePersistence.java
+++ src/main/java/br/com/dio/javaio/NIOFilePersistence.java
@@ -2,6 +2,9 @@ package br.com.dio.javaio;
 
 import java.io.*;
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class NIOFilePersistence implements FilePersistence{
     private final String currentDir = System.getProperty("user.dir");
@@ -34,13 +37,32 @@ public class NIOFilePersistence implements FilePersistence{
     }
 
     @Override
-    public boolean remove(String sentence) {
-        return false;
+    public boolean remove(final String sentence) {
+        var content = findAll();
+        var contentList = Stream.of(content.split(System.lineSeparator())).toList();
+        if (contentList.stream().noneMatch(c -> c.contains(sentence))) return false;
+
+        clearFile();
+        contentList.stream()
+                .filter(c-> !c.contains(sentence))
+                .forEach(this::write);
+
+        return true ;
     }
 
     @Override
     public String replace(final String oldContent, final  String newContent) {
-        return "";
+        var contentList = toListString();
+
+        if (contentList.stream().noneMatch(c -> c.contains(oldContent))) return "";
+
+        clearFile();
+        contentList.stream()
+                .map(c-> c.contains(oldContent) ? newContent : c)
+                .forEach(this::write);
+
+        return newContent ;
+
     }
 
     @Override
@@ -67,9 +89,41 @@ public class NIOFilePersistence implements FilePersistence{
     }
 
     @Override
-    public String findBy(String sentence) {
-        return "";
+    public String findBy(final String sentence) {
+        var content = new StringBuilder();
+        try(var file = new RandomAccessFile(new File(currentDir + storeDir + fileName),"r");
+            var channel = file.getChannel();
+        ){
+
+            var buffer = ByteBuffer.allocate(256);
+            var byteReader = channel.read(buffer);
+            while (byteReader != -1){
+                buffer.flip();
+                while (buffer.hasRemaining()){
+                    while (!content.toString().endsWith(System.lineSeparator())){
+                        content.append((char) buffer.get());
+                    }
+                    if (content.toString().contains(sentence)){
+                        break;
+                    }else {
+                        content.setLength(0);
+                    }
+                    if (!content.isEmpty()) break;
+                }
+                buffer.clear();
+                byteReader = channel.read(buffer);
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return content.toString();
     }
+
+    private List<String> toListString() {
+        var content = findAll();
+        return new ArrayList<>(Stream.of(content.split(System.lineSeparator())).toList());
+    }
+
     private void clearFile(){
         try(OutputStream outputStream = new FileOutputStream(currentDir + storeDir + fileName)) {
 //         System.out.printf("Inicializando recursos (%s) \n " , currentDir + storeDir + fileName);
